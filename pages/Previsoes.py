2e0bdc2e9d70d361b0af9d9075a722a0338457af
Isaque Ramalho Dos Santos <168669233+ISQRS00@users.noreply.github.com>
2024-12-03 01:03:45 -0300

    Update Previsoes.py

--- pages/Previsoes.py
+++ pages/Previsoes.py
@@ -1,18 +1,13 @@
 import streamlit as st
-import requests
-from PIL import Image
-from io import BytesIO
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-from datetime import date, timedelta
-from sklearn.metrics import accuracy_score, r2_score, mean_absolute_error, mean_squared_error
+from datetime import timedelta
 import statsmodels.api as sm
-import yfinance as yf
-from statsmodels.tsa.seasonal import seasonal_decompose
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # Configurações do Streamlit
-
+st.set_page_config(page_title="Deploy | Tech Challenge 4 | FIAP", layout='wide')
 
 # Função para calcular WMAPE
 @st.cache_data
@@ -25,21 +20,21 @@ def load_data():
     df = pd.read_csv('https://raw.githubusercontent.com/ISQRS00/dashboard_tc4/main/barril.csv', sep=';')
     df.drop(columns=['Unnamed: 2'], inplace=True)
     df.rename(columns={'Data': 'data', 'Preço - petróleo bruto - Brent (FOB) - US$ - Energy Information Administration (EIA) - EIA366_PBRENT366': 'realizado'}, inplace=True)
-    df['data'] = pd.to_datetime(df['data'])
+    df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y', dayfirst=True)
     df['realizado'] = df['realizado'].str.replace(',', '.').astype(float)
     df['realizado'] = df['realizado'].ffill()  # Preencher valores ausentes
     return df
 
-# Função para treinar o modelo ETS
-@st.cache_data
-def train_ets_model(train_data, season_length=60):
+# Função para treinar o modelo ETS (utilizar cache de recursos)
+@st.cache_resource
+def train_ets_model(train_data, season_length=252):
     model_ets = sm.tsa.ExponentialSmoothing(train_data['realizado'], seasonal='mul', seasonal_periods=season_length).fit()
     return model_ets
 
 # Função para previsão com o modelo ETS
 @st.cache_data
-def forecast_ets(train, valid, model_ets):
-    forecast_ets = model_ets.forecast(len(valid))
+def forecast_ets(train, valid, _model_ets):  # Modificado para _model_ets
+    forecast_ets = _model_ets.forecast(len(valid))
     forecast_dates = pd.date_range(start=train['data'].iloc[-1] + pd.Timedelta(days=1), periods=len(valid), freq='D')
     ets_df = pd.DataFrame({'data': forecast_dates, 'previsão': forecast_ets})
     ets_df = ets_df.merge(valid, on=['data'], how='inner')
@@ -63,7 +58,7 @@ Escolha o número de dias para o corte e veja como o modelo se comporta para dif
 """)
 
 # Input para o número de dias para corte
-dias_corte = st.number_input('Selecione o número de dias para o corte entre 7 e 90:', min_value=7, max_value=90)
+dias_corte = st.number_input('Selecione o número de dias para o corte entre 7 e 90:', min_value=7, max_value=90, value=7)
 
 # Calcular a data de corte com base no número de dias
 cut_date = df_barril_petroleo['data'].max() - timedelta(days=dias_corte)
@@ -72,12 +67,33 @@ cut_date = df_barril_petroleo['data'].max() - timedelta(days=dias_corte)
 train = df_barril_petroleo.loc[df_barril_petroleo['data'] < cut_date]
 valid = df_barril_petroleo.loc[df_barril_petroleo['data'] >= cut_date]
 
-# Treinar o modelo ETS
-model_ets = train_ets_model(train)
+# Exibir o tamanho dos conjuntos
+st.write(f"A data de corte é: {cut_date}")
+st.write(f"Tamanho do conjunto de treino: {len(train)}")
+st.write(f"Tamanho do conjunto de validação: {len(valid)}")
 
-# Forecast ETS
+# Barra de progresso para o treinamento e previsão
+progress = st.progress(0)
+
+# Treinar o modelo ETS e realizar previsão
 if st.button("Gerar Previsão"):
+    # Atualizar barra de progresso para indicar que o treinamento está em andamento
+    for i in range(30):
+        progress.progress(i + 1)
+    
+    # Treinar o modelo ETS
+    model_ets = train_ets_model(train)
+    
+    # Atualizar a barra de progresso
+    for i in range(30, 60):
+        progress.progress(i + 1)
+    
+    # Prever com o modelo ETS
     ets_df, wmape_ets, MAE_ets, MSE_ets, R2_ets = forecast_ets(train, valid, model_ets)
+    
+    # Atualizar a barra de progresso para 100%
+    for i in range(60, 100):
+        progress.progress(i + 1)
 
     st.subheader('Métricas de Desempenho do Modelo ETS')
     st.write(f'WMAPE: {wmape_ets:.2%}')
@@ -97,7 +113,7 @@ if st.button("Gerar Previsão"):
         yaxis=dict(title="Valor (US$)", tickformat=".3f"),
         autosize=True
     )
-    st.plotly_chart(fig_ets)
+    st.plotly_chart(fig_ets, use_container_width=True)
 
     # Opção de Download dos Resultados
     st.subheader('Baixar Resultados')
